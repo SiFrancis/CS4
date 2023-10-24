30ff0d5f043ff52a4e9476e60f4e7d2d7645cce2
PC21 <PC21@DESKTOP-BU9KNEQ>
2023-10-24 15:01:12 +0800

    completed Automation 1 format and checking

--- project/src/project/project.java
+++ project/src/project/project.java
@@ -41,21 +41,42 @@ public class project {
                                 double b2 = b * b;
                                 double c2 = a2 + b2;
                                 System.out.println("Computation:");
-                                System.out.println("c^2 = a^2 + b^2");
-                                System.out.println("c^2 = " + a + "^2 + " + b + "^2");
-                                System.out.println("c^2 = " + a2 + " + " + b2);
-                                System.out.println("c^2 = " + c2);
-                                System.out.println("c = sqrt(" + c2 + ")");
+                                System.out.println("    c^2 = a^2 + b^2");
+                                System.out.println("    c^2 = " + a + "^2 + " + b + "^2");
+                                System.out.println("    c^2 = " + a2 + " + " + b2);
+                                System.out.println("    c^2 = " + c2);
+                                System.out.println("    c = sqrt(" + c2 + ")");
                                 int root = (int)Math.sqrt(c2);
+                                boolean simplifiable = false;
                                 for (int i = root; i > 1; i--) {
                                     if (c2 == (i*i)) {
-                                        System.out.println("c = " + i);
+                                        System.out.println("    c = " + i);
+                                        System.out.println("Checking:");
+                                        System.out.println("    c^2 = a^2 + b^2");
+                                        System.out.println("    " + i + "^2 = " + a + "^2 + " + b + "^2");
+                                        System.out.println("    " + c2 + " = " + a2 + " + " + b2);
+                                        System.out.println("    " + c2 + " = " + (a2 + b2));
+                                        simplifiable = true;
                                         break;
                                     }
                                     else if (c2 % (i*i) == 0) {
-                                        System.out.println("c = " + i + "sqrt(" + (c2/(i*i)) + ")");
+                                        System.out.println("    c = " + i + "sqrt(" + (c2/(i*i)) + ")");
+                                        System.out.println("Checking:");
+                                        System.out.println("    c^2 = a^2 + b^2");
+                                        System.out.println("    (" + i + "sqrt(" + (c2/(i*i)) + "))^2 = " + a + "^2 + " + b + "^2");
+                                        System.out.println("    " + i + "^2(" + (c2/(i*i)) + ") = " + a2 + " + " + b2);
+                                        System.out.println("    " + (i*i) + "(" + (c2/(i*i)) + ") = " + a2 + " + " + b2);
+                                        System.out.println("    " + c2 + " = " + (a2 + b2));
+                                        simplifiable = true;
                                         break;
                                     }
+                                }
+                                if (simplifiable != true) {
+                                    System.out.println("Checking: ");
+                                    System.out.println("    c^2 = a^2 + b^2");
+                                    System.out.println("    (sqrt(" + c2 + "))^2 = " + a + "^2 + " + b + "^2");
+                                    System.out.println("    " + c2 + " = " + a2 + " + " + b2);
+                                    System.out.println("    " + c2 + " = " + (a2 + b2));
                                 }
                                 break;
                             case 2:
@@ -84,7 +105,14 @@ public class project {
                     while (automActive == true);
                     break;
                 case 2:
-                    // game stuff
+                    scan.nextLine();
+                    System.out.println("==========PYGOMON (name pending)==========");
+                    System.out.println("pokemon but triangle or something like that\n");
+                    System.out.println("======[At the Pygomon Institute]======");
+                    System.out.println("DR. PYTHA: Ah, welcome to the Institute! I see we have another first-timer over here, eh?");
+                    System.out.println("[Press Enter to move to the next dialogue.]");
+                    scan.nextLine();
+                    System.out.println("YOU: Um, good morning! Yes, I actually found this thing blah blah blah");
                     break;
                 case 0:
                     isActive = false;
