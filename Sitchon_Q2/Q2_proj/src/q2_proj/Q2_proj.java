2d8e282a920f9dea06af515d93f82d0da9328dcd
PC C105 <PC C105@PC-C105>
2023-12-13 08:32:42 +0800

    added Q2_project, renamed PE folder to Q2_PEs

---
+++ Sitchon_Q2/Q2_proj/src/q2_proj/Q2_proj.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package q2_proj;
+
+/**
+ *
+ * @author PC C105
+ */
+public class Q2_proj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
