f4443a518f6480e8b0709a7885764c35ba536aba
SiFrancis <103404541+SiFrancis@users.noreply.github.com>
2024-02-19 21:21:28 +0800

    sssss

--- 3rdQ/src/AAPE1/class_defs.java
+++ 3rdQ/src/AAPE1/class_defs.java
@@ -11,21 +11,23 @@ import java.util.HashMap;
  * @author user
  */
 
-abstract class Pygomon {
-//  todo: add smth for the photo, maybe link it to name
+class Pygomon {
     private String name;
     private int level = 1;
     private int max_hp = 15 + 5 * (level - 1);
+    private int current_hp = max_hp;
     private int dmg = 4;
     public HashMap<String, Integer> attackSet = new HashMap<>();
     
     public void setName(String name){this.name = name;}
     public void setLevel(int level) {this.level = level;}
+    public void setCurrentHP(int current_hp) {this.current_hp = current_hp;}
     public void addAttack(String atkName, int atkDmg){
         attackSet.put(atkName, atkDmg);
     }
     
     public String getName(){return this.name;}
     public int getLevel(){return this.level;}
+    public int getCurrentHP(){return this.current_hp;}
     public HashMap getAttacks(){return this.attackSet;}
 }
